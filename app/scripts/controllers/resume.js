7d56a1aefcfa321ec9e58c79d74789198e411712
Adam Booth <adam@ambooth.com>
2014-03-25 22:13:00 -0500

    Print Styling

--- app/scripts/controllers/resume.js
+++ app/scripts/controllers/resume.js
@@ -1,28 +1,12 @@
 'use strict';
 
+/**
+ * @name ResumeCtrl
+ */
 angular.module('amboothcomApp')
     .controller('ResumeCtrl', function ($scope, person) {
         $scope.profile = person.profile;
-
-        var columns = 3;
-        var skillList = [
-            [],
-            [],
-            []
-        ];
-        angular.forEach(person.skills, function (skill, index) {
-//            console.log((index % 3) + 1, index %3);
-                if (index < 5) {
-                    skillList[0].push(skill);
-                } else if (index < 10) {
-                    skillList[1].push(skill);
-                } else {
-                    skillList[2].push(skill);
-                }
-        });
-
-        console.log(skillList);
-
-        $scope.skillLists = skillList;
-        $scope.xp = person.experience;
+        $scope.skillLists = person.skills;
+        $scope.xp = person.xp;
+        $scope.education = person.education;
     });
--- app/scripts/services/person.js
+++ app/scripts/services/person.js
@@ -9,119 +9,178 @@ angular.module('amboothcomApp')
             'with strong organizational skills. Group leader and team player with strong ' +
             'people skills resulting in many successful projects including client websites ' +
             'and web applications.',
-        skills: [
-            'Object Oriented PHP',
-            'FuelPHP MVC Framework',
-            'HTML5',
-            'CSS3',
-            'jQuery',
-            'AngularJS',
-            'MySQL & SQL',
-            'Git / Github',
-            'Amazon AWS',
-            'Linux & Apache Setup/Management',
-            'Regular Expressions (RegEx)',
-            'Wordpress: Themes & Plugins',
-            'Responsive Website Design'
-        ],
-        experience: [
+        education: [
             {
-                start: 'Dec 2012',
-                end: 'Present',
-                title: 'Full Stack Web Developer',
-                company: 'Flogistix LP',
-                location: 'Oklahoma City, OK',
-                text: 'Web application developer assigned as the lead on multiple projects used by corporate staff and ' +
-                    'field mechanics. Introduced Object Oriented practices and optimized pre-existing code bringing ' +
-                    'noticeably faster database interaction speeds. Transitioned existing web applications and websites ' +
-                    'to Amazon EC2 instances and RDS databases. Created internal documentation system and work flows ' +
-                    'using GitHub to maintain company code and push new versions to production servers. Extensive work ' +
-                    'with responsive website design and development. Developed a native Windows 8 touch screen ' +
-                    'application using WinJS frameworks used by manufacturing workers to track their time.',
-                items: [
-                    'Amazon AWS/Linux administrator',
-                    'Web Developer using FuelPHP',
-                    'Created internal documentation system',
-                    'Heavy use of Git/Github',
-                    'Created a Windows 8 application using Windows Javascript Frameworks'
-                ]
-            },
-            {
-                start: 'Feb 2012',
-                end: 'Nov 2012',
-                title: 'Internal Developer',
-                company: 'LifeChurch.tv',
+                start: '2007',
+                end: '2012',
+                title: 'Oklahoma Christian University',
                 location: 'Edmond, OK',
-                text: 'Web developer position that focused on internal projects used by staff ' +
-                    'and volunteers. Utilized the FuelPHP Framework and Git/Github for versioning ' +
-                    'and collaboration. Worked with numerous REST-based APIs including Google Maps, ' +
-                    'Fellowship One, the CellForce SMS system, and Active Directory. Created and ' +
-                    'maintained Wordpress websites and plugins used by team members.',
-                items: [
-                    'Used FuelPHP Framework and Git/Github',
-                    'Used APIs for Google Maps, Fellowship One, CellForce SMS systems, and Active Directory interactions',
-                    'Created web applications used by staff and volunteers',
-                    'Created and maintain Wordpress websites and plugins'
-                ]
-            },
-            {
-                start: 'April 2010',
-                end: 'Sept 2011',
-                title: 'Web Developer',
-                company: 'T&S Web Design',
-                location: 'Edmond, OK',
-                text: 'Lead developer and go-to guy for technical support questions and issues. Gracefully ' +
-                    'transitioned company into using Google Apps for email, calendars, and their internal wiki ' +
-                    'system.',
-                items: [
-                    'Created websites using WordPress framework, HTML, PHP, CSS, and JQuery based on designs made in-house',
-                    'Conducted training meetings for clients',
-                    'Provided technical support for staff and clients',
-                    'On-site technical support and training sessions for clients'
-                ]
-            },
-            {
-                start: 'April 2010',
-                end: 'May 2010',
-                title: 'Programmer Intern',
-                company: 'North Institute, Oklahoma Christian University',
-                location: 'Edmond, OK',
-                text: 'Student intern position. Made modifications to the organization website making it easier to ' +
-                    'navigate and find content. Further developed internal media sharing system used to distribute ' +
-                    'content to students.',
-                items: [
-                    'Redesigned and managed North Institute website',
-                    'Worked on internal media sharing software used to distribute videos and other media to students using C#, ASP, and HTML'
-                ]
-            },
-            {
-                start: 'Aug 2009',
-                end: 'April 2009',
-                title: 'Software Technician',
-                company: 'Support Central, Oklahoma Christian University',
-                location: 'Edmond, OK',
-                text: 'Software Support Technician at Oklahoma Christian University Provided friendly support ' +
-                    'to students, teachers and staff. Adapted quickly to workflow, mentored new and current student ' +
-                    'workers.'
-            },
-            {
-                start: 'May 2009',
-                end: 'Aug 2009',
-                title: 'Computer Specialist',
-                company: 'iCafe',
-                location: 'Lawrence, KS',
-                text: 'Apple technical support specialist who provided support for clients on the phone and in person. ' +
-                    'Diagnosed and repaired hardware and software issues on Macintosh computers.'
-            },
-            {
-                start: '',
-                end: '',
-                title: '',
-                company: '',
-                location: '',
-                text: ''
+                major: 'Information Systems Management',
+                text: 'Graduated April 2012'
             }
-        ]
+        ],
+        skills: {
+            column1: [
+                'Object Oriented PHP',
+                'FuelPHP MVC Framework',
+                'NodeJS',
+                'AngularJS'
+            ],
+            column2: [
+                'jQuery',
+                'HTML5, CSS3',
+                'MySQL & SQL',
+                'Git / Github'
+            ],
+            column3: [
+                'Amazon AWS',
+                'Regular Expressions',
+                'Wordpress Themes'
+            ]
+        },
+        xp: {
+            'Experience': [
+                {
+                    start: 'Dec 2012',
+                    end: 'Present',
+                    title: 'Full Stack Web Developer',
+                    company: 'Flogistix LP',
+                    location: 'Oklahoma City, OK',
+                    text: 'Full Stack mobile & web applications developer. Played major role in optimizing  used by corporate staff and ' +
+                        'field mechanics. Introduced Object Oriented practices and optimized pre-existing code bringing ' +
+                        'noticeably faster database interaction speeds. Transitioned existing web applications and websites ' +
+                        'to Amazon EC2 instances and RDS databases. Created internal documentation system and work flows ' +
+                        'using GitHub to maintain company code and push new versions to production servers. Extensive work ' +
+                        'with responsive website design and development.',
+                    items: [
+                        'Amazon AWS/Linux administrator',
+                        'Web Developer using FuelPHP',
+                        'Created internal documentation system',
+                        'Heavy use of Git/Github',
+                        'Created a Windows 8 application using Windows Javascript Frameworks'
+                    ]
+                },
+                {
+                    start: 'Feb 2012',
+                    end: 'Nov 2012',
+                    title: 'Internal Developer',
+                    company: 'LifeChurch.tv',
+                    location: 'Edmond, OK',
+                    text: 'Web developer position that focused on internal projects used by staff ' +
+                        'and volunteers. Utilized the FuelPHP Framework and Git/Github for versioning ' +
+                        'and collaboration. Worked with numerous REST-based APIs including Google Maps, ' +
+                        'Fellowship One, the CellForce SMS system, and Active Directory. Created and ' +
+                        'maintained Wordpress websites and plugins used by team members.',
+                    items: [
+                        'Used FuelPHP Framework and Git/Github',
+                        'Used APIs for Google Maps, Fellowship One, CellForce SMS systems, and Active Directory interactions',
+                        'Created web applications used by staff and volunteers',
+                        'Created and maintain Wordpress websites and plugins'
+                    ]
+                },
+                {
+                    start: 'April 2010',
+                    end: 'Sept 2011',
+                    title: 'Web Developer',
+                    company: 'T&S Web Design',
+                    location: 'Edmond, OK',
+                    text: 'Lead developer and go-to guy for technical support questions and issues. Gracefully ' +
+                        'transitioned company into using Google Apps for email, calendars, and their internal wiki ' +
+                        'system.',
+                    items: [
+                        'Created websites using WordPress framework, HTML, PHP, CSS, and JQuery based on designs made in-house',
+                        'Conducted training meetings for clients',
+                        'Provided technical support for staff and clients',
+                        'On-site technical support and training sessions for clients'
+                    ]
+                },
+                {
+                    start: 'April 2010',
+                    end: 'May 2010',
+                    title: 'Programmer Intern',
+                    company: 'North Institute, OCU',
+                    location: 'Edmond, OK',
+                    text: 'Student intern position. Made modifications to the organization website making it easier to ' +
+                        'navigate and find content. Further developed internal media sharing system used to distribute ' +
+                        'content to students.',
+                    items: [
+                        'Redesigned and managed North Institute website',
+                        'Worked on internal media sharing software used to distribute videos and other media to students using C#, ASP, and HTML'
+                    ]
+                },
+                {
+                    start: 'Aug 2009',
+                    end: 'April 2010',
+                    title: 'Software Technician',
+                    company: 'Support Central, OCU',
+                    location: 'Edmond, OK',
+                    text: 'Software Support Technician at Oklahoma Christian University Provided friendly support ' +
+                        'to students, teachers and staff. Adapted quickly to workflow, mentored new and current student ' +
+                        'workers.',
+                    items: [
+                        'Provided in-person technical support to fellow students, teachers and staff',
+                        'Provided support for teachers in classroom (On and off the clock)',
+                        'Minor hardware diagnostics before passing computer to hardware department if necessary'
+                    ]
+                },
+                {
+                    start: 'May 2009',
+                    end: 'Aug 2009',
+                    title: 'Computer Specialist',
+                    company: 'iCafe',
+                    location: 'Lawrence, KS',
+                    text: 'Apple technical support specialist who provided support for clients on the phone and in person. ' +
+                        'Diagnosed and repaired hardware and software issues on Macintosh computers.',
+                    items: [
+                        'Provided in-person and phone support to clients',
+                        'Diagnosed and repaired Apple computers with hardware or software issues'
+                    ]
+                }
+            ],
+            'Other Experience': [
+                {
+                    start: 'Sept 2008',
+                    end: 'Apr 2012',
+                    title: 'Executive Secretary/Webmaster',
+                    company: 'Psi Epsilon Fraternity',
+                    location: 'Oklahoma Christian University, Edmond, OK',
+                    text: 'Founded new fraternity, Psi Epsilon, during freshmen year in college. Grew fraternity to over ' +
+                        '70 members before graduation. Highly involved with all decision-making and served as an ' +
+                        'executive officer for 4 years. Created the website used by all members for information and ' +
+                        'even payment of dues. Maintained emailing lists and overall organization of club which included ' +
+                        'taking meeting notes and sending calendar invites.',
+                    items: [
+                        'Founded club and recruited 20 initial members which grew to 70+ by graduation',
+                        'Created and maintained fraternity website',
+                        'In charge of taking meeting notes and maintaining emailing lists'
+                    ]
+                },
+                {
+                    start: 'Sept 2007',
+                    end: 'Apr 2009',
+                    title: 'Male Class Representative',
+                    company: 'OC Student Government Association',
+                    location: 'Oklahoma Christian University, Edmond, OK',
+                    text: 'On team of dedicated officers who met weekly and planned events to bring the students closer ' +
+                        'together. Planned two banquets, two school-wide parties, and attended many student government ' +
+                        'meetings on a weekly basis. Voted on significant developments and issues that affected the ' +
+                        'entire school. Managed the SGA website which was used by the entire school.',
+                    items: [
+                        'Attended weekly SGA meetings as well as separate freshmen officer meetings',
+                        'Planned two banquets and two school-wide parties',
+                        'Maintained SGA website for two years'
+                    ]
+                }
+//                {
+//                    start: '',
+//                    end: '',
+//                    title: '',
+//                    company: '',
+//                    location: '',
+//                    text: ''
+//                }
+            ]
+        }
 
     })
 ;
